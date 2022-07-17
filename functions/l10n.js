2972a17cef6322931cac195b6d40191ee0630a72
taka-2120 <yu_tkhs@icloud.com>
2022-07-17 23:53:29 +0900

    renew: portfolio

---
+++ functions/l10n.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded', function(){
+  const userLocale =
+  navigator.languages && navigator.languages.length
+    ? navigator.languages[0]
+    : navigator.language;
+  var glot = new Glottologist();
+  glot.import("data/l10n.json").then(() => {
+    glot.render();
+  });
+});
