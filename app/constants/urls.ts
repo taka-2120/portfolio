3d474344408c9dea4398dd11393ee948e80fee3b
taka-2120 <yu_tkhs@icloud.com>
2024-05-04 00:58:17 +0900

    refactor: replace and move constants

--- app/constants/urls.ts
+++ app/constants/urls.ts
@@ -1,5 +1,7 @@
-export const sharedBucket = 'shared';
-export const privacyPolicyEnUrl = 'privacy_policy_en.md';
-export const privacyPolicyJaUrl = 'privacy_policy_ja.md';
-export const termsAndConditionsEnUrl = 'terms_and_conditions_en.md';
-export const termsAndConditionsJaUrl = 'terms_and_conditions_ja.md';
+export enum Urls {
+  sharedBucket = 'shared',
+  privacyPolicyEn = 'privacy_policy_en',
+  privacyPolicyJa = 'privacy_policy_ja',
+  termsAndConditionsEn = 'terms_and_conditions_en',
+  termsAndConditionsJa = 'terms_and_conditions_ja',
+}
