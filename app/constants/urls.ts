f4b459fdd9393d8be4f8f79b4be5aeb983bcbd3d
taka-2120 <yu_tkhs@icloud.com>
2024-05-04 18:49:09 +0900

    fix: markdown file name

--- app/constants/urls.ts
+++ app/constants/urls.ts
@@ -1,7 +1,7 @@
 export enum Urls {
   sharedBucket = 'shared',
-  privacyPolicyEn = 'privacy_policy_en',
-  privacyPolicyJa = 'privacy_policy_ja',
-  termsAndConditionsEn = 'terms_and_conditions_en',
-  termsAndConditionsJa = 'terms_and_conditions_ja',
+  privacyPolicyEn = 'privacy_policy_en.md',
+  privacyPolicyJa = 'privacy_policy_ja.md',
+  termsAndConditionsEn = 'terms_and_conditions_en.md',
+  termsAndConditionsJa = 'terms_and_conditions_ja.md',
 }
