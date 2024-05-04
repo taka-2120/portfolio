27af860bc2ee781c53b8d8fda0a4b23e4ed40565
taka-2120 <yu_tkhs@icloud.com>
2024-05-04 14:17:00 +0900

    refactor: modify constants

---
+++ app/constants/imageUrl.ts
@@ -0,0 +1,29 @@
+export enum ImageUrl {
+  supabase = '/skills/supabase.png',
+  swift = '/skills/swift.png',
+  flutter = '/skills/flutter.png',
+  react = '/skills/react.png',
+  ts = '/skills/ts.png',
+  js = '/skills/js.png',
+  node = '/skills/node.png',
+  python = '/skills/python.png',
+  clang = '/skills/clang.png',
+  firebase = '/skills/firebase.png',
+  nextjs = '/skills/nextjs.svg',
+  wordventure = '/projects/wordventure.png',
+  portrait = '/portrait.png',
+}
+
+export const skills: ImageUrl[] = [
+  ImageUrl.swift,
+  ImageUrl.flutter,
+  ImageUrl.react,
+  ImageUrl.nextjs,
+  ImageUrl.ts,
+  ImageUrl.js,
+  ImageUrl.node,
+  ImageUrl.python,
+  ImageUrl.clang,
+  ImageUrl.firebase,
+  ImageUrl.supabase,
+];
