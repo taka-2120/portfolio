ec89b54b6da8f812b9298f071616dd3f843ca861
taka-2120 <yu_tkhs@icloud.com>
2025-03-03 02:31:59 +0900

    feat: experiences supports localizations

--- src/entities/experience.ts
+++ src/entities/experience.ts
@@ -1,19 +1,26 @@
 interface ExperienceProps {
-  name: string;
-  role: string;
+  nameEn: string;
+  roleEn: string;
+  nameJa: string;
+  roleJa: string;
   start: Date;
   end?: Date | null;
 }
 
 class Experience {
-  name: string;
-  role: string;
+  nameEn: string;
+  roleEn: string;
+  nameJa: string;
+  roleJa: string;
   start: Date;
   end: Date | null;
 
-  constructor({ name, role, start, end = null }: ExperienceProps) {
-    this.name = name;
-    this.role = role;
+  constructor({ nameEn, roleEn, nameJa, roleJa, start, end = null }: ExperienceProps) {
+    this.nameEn = nameEn;
+    this.roleEn = roleEn;
+    this.nameJa = nameJa;
+    this.roleJa = roleJa;
+    
     this.start = start;
     this.end = end;
   }
