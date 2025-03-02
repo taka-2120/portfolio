6d8b8f7060efa53ea37caf0c962333802c72297a
taka-2120 <yu_tkhs@icloud.com>
2025-03-03 02:32:37 +0900

    chore: modify existing projects' privacy policy

---
+++ src/app/[lang]/services/beanslife/privacy-policy/page.tsx
@@ -0,0 +1,15 @@
+"use server";
+
+import { getDictionary } from "@/app/[lang]/dictionaries";
+import JA from "./ja.mdx";
+import EN from "./en.mdx";
+import Wrapper from "@/components/custom/wrapper";
+
+const TMPrivacyPolicy = async ({ params }: { params: Promise<{ lang: 'en' | 'ja' }> }) => {
+	const { lang } = await params;
+	const dict = await getDictionary(lang);
+
+	return <Wrapper>{lang === "ja" ? <JA /> : <EN />}</Wrapper>;
+};
+
+export default TMPrivacyPolicy;
--- src/app/[lang]/services/econotify/privacy-policy/page.tsx
+++ src/app/[lang]/services/econotify/privacy-policy/page.tsx
@@ -4,7 +4,7 @@ import JA from "./ja.mdx";
 import EN from "./en.mdx";
 import Wrapper from "@/components/custom/wrapper";
 
-const ENPrivacyPolicy = async ({ params }: { params: { lang: string } }) => {
+const ENPrivacyPolicy = async ({ params }: { params: Promise<{ lang: 'en' | 'ja' }> }) => {
 	const { lang } = await params;
 
 	return <Wrapper>{lang === "ja" ? <JA /> : <EN />}</Wrapper>;
